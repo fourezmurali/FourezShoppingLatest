7c62f2680e21e4417e8266dcca44da71e8efba84
MuraliTharan.A <amurali123@gmail.com>
2018-04-12 19:10:35 +0530

    Commit

--- app/src/main/java/com/example/hp/fourezshopping/MainActivity.java
+++ app/src/main/java/com/example/hp/fourezshopping/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 
 import com.example.hp.fourezshopping.NavigationHome.NavigationHome;
 
-//testing
+//testingMurali
 public class MainActivity extends Activity {
     Button signin;
 
